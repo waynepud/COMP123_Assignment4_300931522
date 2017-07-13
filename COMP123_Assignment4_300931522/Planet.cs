e3744725fce1f03de9600cf8dcefd5fb9fef7da1
Wayne <Wayne@DESKTOP-CDRCGMN>
2017-07-13 00:21:49 -0400

    Created the solution for assignment 4 and created the planet abstract class

---
+++ COMP123_Assignment4_300931522/Planet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Wayne Pud
+ * Date: July 13, 2017
+ * Description: This is the planet abstract class
+ * Version: 0.1 - Created solution and planet abstract class
+ */
+
+namespace COMP123_Assignment4_300931522
+{
+    /// <summary>
+    /// This is the planet abstract class
+    /// </summary>
+    class Planet
+    {
+        // private instance variables (fields)
+        
+
+        // public properties
+
+        // constructors -----------------------------------------------------------
+
+        // private methods
+
+        // public methods
+    }
+}
--- COMP123_Assignment4_300931522/Program.cs
+++ COMP123_Assignment4_300931522/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Wayne Pud
+ * Date: July 13, 2017
+ * Description: Assignment 4 - Abstract planets
+ * Version: 0.1 - Created solution and planet abstract class
+ */
+
 namespace COMP123_Assignment4_300931522
 {
     class Program
