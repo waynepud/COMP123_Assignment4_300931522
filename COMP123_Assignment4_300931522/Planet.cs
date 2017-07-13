a68015a3e4ee38c6813c3fabec1391229e0f8e0f
Wayne <Wayne@DESKTOP-CDRCGMN>
2017-07-13 00:35:27 -0400

    Added private instance variables and public properties to the planet abstract class

--- COMP123_Assignment4_300931522/Planet.cs
+++ COMP123_Assignment4_300931522/Planet.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Wayne Pud
  * Date: July 13, 2017
  * Description: This is the planet abstract class
- * Version: 0.1 - Created solution and planet abstract class
+ * Version: 0.2 - Added private instance variables and public properties to the planet abstract class
  */
 
 namespace COMP123_Assignment4_300931522
@@ -19,9 +19,88 @@ namespace COMP123_Assignment4_300931522
     class Planet
     {
         // private instance variables (fields)
-        
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
 
         // public properties
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }   
+
 
         // constructors -----------------------------------------------------------
 
