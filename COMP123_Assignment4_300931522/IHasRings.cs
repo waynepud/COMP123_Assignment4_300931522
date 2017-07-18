f31ab51b57ce3c55a64f487d2668488e1fb2eb81
Wayne <Wayne@DESKTOP-CDRCGMN>
2017-07-18 15:47:45 -0400

    Created IHasRings interface and HasRings method

---
+++ COMP123_Assignment4_300931522/IHasRings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Wayne Pud
+ * Date: July 18, 2017
+ * Description: This is the IHasRings interface
+ * Version: 0.1 - Created the IHasRings interface and HasRings() method
+ */
+
+namespace COMP123_Assignment4_300931522
+{
+    /// <summary>
+    /// This is the IHasRings interface
+    /// </summary>
+    public interface IHasRings
+    {
+        //PUBLIC METHODS
+        bool HasRings();
+    }
+}
