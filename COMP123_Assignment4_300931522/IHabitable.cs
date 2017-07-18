d53a24c3b7844b8ea8e1a11d3410dc3c0393e05e
Wayne <Wayne@DESKTOP-CDRCGMN>
2017-07-18 15:53:24 -0400

    Created IHabitable interface and added Habitable method

---
+++ COMP123_Assignment4_300931522/IHabitable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Wayne Pud
+ * Date: July 18, 2017
+ * Description: This is the IHabitable interface
+ * Version: 0.1 - Created the IHabitable interface and added the Habitable method
+ */
+
+namespace COMP123_Assignment4_300931522
+{
+    /// <summary>
+    /// This is the IHabitable interface
+    /// </summary>
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
