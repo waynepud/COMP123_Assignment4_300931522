359bd1864925d83e09ae8f34c36d8c00c3a26340
Wayne <Wayne@DESKTOP-CDRCGMN>
2017-07-28 15:34:38 -0400

    Added test harness for driver class to test HasMoons, HasRings andHabitable methods, also edited WaitForAnyKey method due to a wild bunny

--- COMP123_Assignment4_300931522/Program.cs
+++ COMP123_Assignment4_300931522/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Wayne Pud
  * Date: July 26, 2017
  * Description: Assignment 4 - Abstract planets
- * Version: 0.3 - Edited ToString object calls
+ * Version: 0.4 - Added test harness for HasMoons, HasRings and Habitable methods
  */
 
 namespace COMP123_Assignment4_300931522
@@ -24,12 +24,18 @@ namespace COMP123_Assignment4_300931522
             GiantPlanet giantplanet = new GiantPlanet("Jupiter", 139822, 1.898 * Math.Pow(10, 27), "Gas");
             Console.WriteLine(giantplanet.ToString());
 
-            TerrestrialPlanet terrestrialplanet = new TerrestrialPlanet("Earth", 12742, 5.972 * Math.Pow(10,24), true);
+            //Extra Work: to test the HasMoons, HasRings and Habitable methods
+            giantplanet.MoonCount = 1;                                                                      // Assigned 1 moon to giantplanet object for testing
+            Console.WriteLine("Does {0} have any moons? {1}", giantplanet.Name, giantplanet.HasMoons());                                   
+            Console.WriteLine("Does {0} have any rings? {1}", giantplanet.Name, giantplanet.HasRings());         // Did not assign any rings to giantplanet
+
+            Console.WriteLine();
+            TerrestrialPlanet terrestrialplanet = new TerrestrialPlanet("Earth", 12742, 5.972 * Math.Pow(10, 24), true);
             Console.WriteLine(terrestrialplanet.ToString());
 
-            // To test the HasMoons, HasRings and Habitable methods
-
-
+            //Extra Work: to test the HasMoons, HasRings and Habitable methods                                             
+            Console.WriteLine("Does {0} have any moons? {1}", terrestrialplanet.Name, terrestrialplanet.HasMoons());        // Did not assign any moons to terrestrialplanet
+            Console.WriteLine("Is {0} habitable (does it have oxygen?) {1}", terrestrialplanet.Name, terrestrialplanet.Habitable());
 
 
             WaitForAnyKey();
@@ -39,7 +45,15 @@ namespace COMP123_Assignment4_300931522
 
         static void WaitForAnyKey()
         {
-            Console.WriteLine("\nPress any input key to continue:");
+           
+            Console.WriteLine("\nKeep pressing a key!!!");
+            Console.WriteLine("A wild bunny has appeared!");
+            System.Console.ReadKey(true);
+            Console.WriteLine("      /)/)");
+            System.Console.ReadKey(true);
+            Console.WriteLine("     (.__.)");
+            System.Console.ReadKey(true);
+            Console.WriteLine("    c(\")(\")");
             Console.ReadKey();
         }
     }
